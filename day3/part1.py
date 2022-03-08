6071f6a17caeeb016036f4e877d15903eae51def
Papirosska <90559167+ChmyrevVladislav@users.noreply.github.com>
2022-03-09 00:25:46 +0700

    Update part1.py

--- day3/part1.py
+++ day3/part1.py
@@ -2,24 +2,24 @@ f1 = open('input.txt', 'r')
 f2 = open('output1.txt', 'w')
 text = f1.read()
     
-xy1 = [[0,0]]
-x1 = 0
-y1 = 0 
+xy = [[0,0]]
+x = 0
+y = 0 
 p = 1
 
 #Обычный санта
 for i in text:
     if i == '^':
-        x1 += 1
+        x += 1
     if i == 'v':
-        x1 -= 1
+        x -= 1
     if i == '>':
-        y1 += 1
+        y += 1
     if i == '<':
-        y1 -= 1
-    if [x1,y1] not in xy1:
+        y -= 1
+    if [x,y] not in xy:
         p += 1
-    xy1.append([x1,y1])
+    xy.append([x,y])
     
 f2.write(str(p))
 f2.close()
