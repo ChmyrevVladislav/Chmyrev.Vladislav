12840350bd6cdc0b74724b356f6dabe61ed42f4c
Papirosska <90559167+ChmyrevVladislav@users.noreply.github.com>
2022-03-08 23:57:22 +0700

    Update part1.py

--- day3/part1.py
+++ day3/part1.py
@@ -1,21 +1,14 @@
 f1 = open('input.txt', 'r')
 f2 = open('output2.txt', 'w')
 text = f1.read()
-t1 = []
-t2 = text[::2]
-for i in text:
-    if i not in t2:
-        t1.append(i)
     
 xy1 = [[0,0]]
 x1 = 0
-y1 = 0
-x2 = 0
-y2 = 0
+y1 = 0 
 p = 1
 
 #Обычный санта
-for i in t1:
+for i in text:
     if i == '^':
         x1 += 1
     if i == 'v':
@@ -26,23 +19,8 @@ for i in t1:
         y1 -= 1
     if [x1,y1] not in xy1:
         p += 1
-    xy1.append([x2,y2])
-
-#Робо-Санта
-for i in t2:
-    if i == '^':
-        x2 += 1
-    if i == 'v':
-        x2 -= 1
-    if i == '>':
-        y2 += 1
-    if i == '<':
-        y2 -= 1
-    if [x2,y2] not in xy1:
-        p += 1
-        xy1.append([x2,y2])
+    xy1.append([x1,y1])
     
 f2.write(str(p))
 f2.close()
 f1.close()
-    
