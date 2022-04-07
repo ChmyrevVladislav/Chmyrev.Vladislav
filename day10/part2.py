1be84accdf9436f41e6ef239fa2bdb02adbb142f
Papirosska <90559167+ChmyrevVladislav@users.noreply.github.com>
2022-04-07 17:54:32 +0700

    Add files via upload

---
+++ day10/part2.py
@@ -0,0 +1,23 @@
+f1 = open("input.txt", "r")
+text = f1.read()
+f2 = open("output2.txt", "w")
+
+def game(text):
+    stroka = ''
+    count = 1
+    for i in range(len(text)-1):
+        if text[i] == text[i+1]:
+            count += 1
+        else:
+            stroka += str(count) + text[i]
+            count = 1
+    return stroka
+
+for i in range (50):
+    text += 'p'
+    total = game(text)
+    text = total
+
+f2.write(str(len(total)))
+f2.close
+f1.close
